500d00f563ee48a42149cf8d750f87516bb02db1
steelydanrules <karl.konetsky@gmail.com>
2020-06-03 09:36:33 -0400

    fixed the webpack errors

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -1,6 +1,6 @@
 import { postUser,
 postSession,
-deleteSession } from '..utils/session';
+deleteSession } from '../util/session';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
